f7897c33e5b068fea33985a2dff341ac41620a0f
Gabi-Acquah <gabiacquah@gmail.com>
2025-03-19 10:31:45 +0000

    course info

---
+++ App.jsx
@@ -0,0 +1,48 @@
+const Header =(props)=>{
+  console.log(props.course)
+  return (
+    <>
+      <h1>Title: {props.course} </h1>
+    </>
+  )
+}
+const Content =(props)=>{
+  console.log(props.part)
+  return (
+    <div>
+      <p> {props.part} {props.exercises} </p>
+    </div>
+  )
+}
+const Total =(props)=>{
+  console.log(props.exercises)
+  return (
+    <p>
+      Number of exercises {props.exercises}
+    </p>
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} exercises={exercises1}/>
+      <Content part={part2} exercises={exercises2}/>
+      <Content part={part3} exercises={exercises3}/>
+
+      <Total exercises = {exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
